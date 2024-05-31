2bb0b1ce97b4ff299b24aeaf1fbe916fa1c513dd
Pham Quan <hanhngao3010@gmail.com>
2024-05-31 09:01:40 +0700

    add middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,20 +2,26 @@ import { NextRequest, NextResponse } from "next/server";
 import { jwtDecode } from "jwt-decode";
 
 export function middleware(req:NextRequest){
+    //next request
     const response = NextResponse.next()
+    //get token
     const token = req.cookies.get("token")
     try{
         const decoded:any = jwtDecode(token?.value+"");
         console.log(decoded.role==="admin");
-        
+        //is validtoken
         if (decoded.exp * 1000 < Date.now()) {
             return NextResponse.redirect(new URL("/login", req.url))
         }
-        if(decoded.role == "admin" && req.nextUrl.pathname !=="/dashboard"){
+        if(decoded.role == "admin"){
+            if(!req.nextUrl.pathname.includes("/dashboard"))
             return NextResponse.redirect(new URL("/dashboard", req.url))
+            else return response
         }
     }catch(e){
-        if(!token && req.nextUrl.pathname != "/dashboard"){
+        console.log(req.nextUrl.pathname);
+        // next request if do not token and is dashboard pages
+        if(!token && !req.nextUrl.pathname.includes("/dashboard")){
             return response;
         }
         console.log("invalid token");
@@ -26,5 +32,10 @@ export function middleware(req:NextRequest){
 }
 
 export const config = {
-    matcher:["/dashboard", "/login", "/register"]
+    matcher:[
+    "/dashboard", 
+    "/login", 
+    "/register",
+    "/dashboard/:path*",
+    ]
 }
